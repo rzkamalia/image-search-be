d36b3c1dc6cd8d00dbf47f35d6023d7d48c40dde
rizka-populix <rizka@populix.co>
2024-09-22 17:07:56 +0700

    :rocket: change host to 0.0.0.0

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ db = Database(collection_name = COLLECTION_NAME)
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ["http://localhost:3000"],
+    allow_origins = ["http://0.0.0.0:3000"],
     allow_credentials = True,
     allow_methods = ["*"],
     allow_headers = ["*"],
